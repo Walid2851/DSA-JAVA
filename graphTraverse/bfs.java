1d9513d7be24a7ad208142a241365d1d856029d3
Mahamud Hasan Walid <130084838+Walid2851@users.noreply.github.com>
2024-09-25 19:55:06 +0600

    add bfs

---
+++ graphTraverse/bfs.java
@@ -0,0 +1,15 @@
+package graphTraverse;
+
+import java.util.Vector;
+
+class bfsTraverse{
+   Vector<Integer> v = new Vector<>(100000);
+   Vector[] g = new Vector[100000];
+
+}
+
+public class bfs{
+    public static void main(String args[]){
+       // System.out.println("hello world");
+    }
+}
